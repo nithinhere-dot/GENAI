1815bfb83bd07d9044d5251fee6df78a792f0c7d
Nithin Kumar <nithin_22b226cs@gecwyd.ac.in>
2026-09-21 09:18:31 +0530

    memory storeed in device

--- apps/2_google_agent.py
+++ apps/2_google_agent.py
@@ -2,16 +2,19 @@ from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
 from context.dev import ContextDev
+from langgraph.checkpoint.memory import InMemorysaver
 import os
 
 
 load_dotenv()
 client = ContextDev()
+memory=InMemorysaver()
 llm=ChatGoogleGenerativeAI(model="gemini-3.6-flash")
 agent = create_agent(
     model=llm,
     tools=[client.web.web_scrape_md],
-    system_prompt="Your an agent and search for any question on google"
+    system_prompt="Your an agent and search for any question on google",
+    checkpointer=memory
 
 )
 
@@ -27,6 +30,6 @@ while True:
                 "content": query
             }
         ]
-    })
+    },{"configurable":{"thread_id":"1"}})
     print("Ai:",response["messages"][-1].text)
 
